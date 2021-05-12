1a864281bffe8dc26d980b816b8a61374a1e214d
Helena <helena.kloosterman@intel.com>
2021-05-12 16:57:49 +0200

    More helpful error message for launch_notebooks script

--- launch_notebooks.py
+++ launch_notebooks.py
@@ -22,17 +22,27 @@ else:
     # If openvino_env is not activated, search for the openvino_env folder in the
     # current and parent directory and launch the notebooks
     try:
-        pythonpath = os.path.normpath(os.path.join(curdir, f"openvino_env/{scripts_dir}/python"))
+        pythonpath = os.path.normpath(
+            os.path.join(curdir, f"openvino_env/{scripts_dir}/python")
+        )
         subprocess.run([pythonpath, "-m", "jupyterlab", "notebooks"])
     except:
         try:
-            pythonpath = os.path.normpath(os.path.join(parentdir, f"openvino_env/{scripts_dir}/python"))
+            pythonpath = os.path.normpath(
+                os.path.join(parentdir, f"openvino_env/{scripts_dir}/python")
+            )
             subprocess.run([pythonpath, "-m", "jupyterlab", "notebooks"])
         except:
-            pythonpath = str(Path(f"../openvino_env/{scripts_dir}/python"))
             print(pythonpath)
             print(
-                "openvino_env could not be found. Please activate the openvino_env virtual "
-                "environment manually and try again."
+                "openvino_env could not be found in the current or parent "
+                "directory, or the installation is not complete. Please follow "
+                "the instructions on "
+                "https://github.com/openvinotoolkit/openvino_notebooks to "
+                "install the notebook requirements in a virtual environment.\n\n"
+                "You can also launch the notebooks without this script by "
+                "activating the virtual environment manually (see step 3 in the "
+                "github page linked above) and typing `jupyter lab notebooks`.\n\n"
+                f"Current directory: {curdir}"
+                f"Python executable: {sys.executable}"
             )
-
